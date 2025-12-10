cd90df501c8921bf9c30e73c44a7d620c65c725f
Bedirhan SAY <bedirhan.sayy@gmail.com>
2025-12-10 16:07:15 +0300

    b

--- src/config/cors.config.ts
+++ src/config/cors.config.ts
@@ -1,14 +1,11 @@
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
-import { ConfigService } from '@nestjs/config';
 
-export function createCorsConfig(configService: ConfigService): CorsOptions {
-  const originsEnv = configService.get<string>('ALLOWED_ORIGINS') ?? '';
-  const allowedOrigins = originsEnv.split(',').map((o) => o.trim());
-
+export function createCorsConfig(): CorsOptions {
   return {
     origin: '*',
+    credentials: false,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'x-company-id', 'x-correlation-id'],
-    exposedHeaders: ['Authorization', 'x-correlation-id'],
+    exposedHeaders: ['x-correlation-id'],
   };
 }
